a32b6e7b71ef8075a527382eb13c8d0bfc1a6d0c
Ismael Ochoa <ismael.ochoa@mindstronghealth.com>
2022-07-18 19:09:41 -0500

    added tercer hola method, updated segundo hola

--- s.py
+++ s.py
@@ -3,5 +3,9 @@ def hola(nombre):
 
 
 def segundo_hola(nombre):
-	print f"adios {nombre}"
+	var saludo = "hasta la vista"
+	print f"adios {nombre} {saludo}"
+
+def tercer_hola(nombre):
+	print f"buenas tardes {nombre}"
 
